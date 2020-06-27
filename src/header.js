84a6fbc28ef17055cdca4b165cca4c9b8fa55bad
kashyapsavaliya <kkashyap.savaliya@gmail.com>
2020-06-27 13:55:49 -0400

    Add image to header and add gallery project

--- src/header.js
+++ src/header.js
@@ -4,6 +4,8 @@ import resume from "./images/file.svg";
 import linkedin from "./images/linkedin.svg";
 import twitter from "./images/twitter.svg";
 import github from "./images/github.svg";
+import blog from "./images/blog.svg"
+import mountain from "./images/mountain.png";
 
 export const Header = () => {
     return (
@@ -21,10 +23,12 @@ export const Header = () => {
                                 <li id="two" className="buttons hvr-grow2"><a href="https://www.github.com/kashyapsavaliya"><img src={github} height="28px" alt="github"></img></a></li>
                                 <li id="three" className="buttons hvr-grow2"><a href="https://www.linkedin.com/in/kashyapsavaliya/"><img src={linkedin} height="28px" alt="linkedin"></img></a></li>
                                 <li id="four" className="buttons hvr-grow2"><a href="https://www.twitter.com/kashyapcode"><img src={twitter} height="28px" alt="twitter"></img></a></li>
+                                <li id="five" className="buttons hvr-grow2"><a href="https://www.linkedin.com/in/kashyapsavaliya/"><img src={blog} height="28px" alt="blog"></img></a></li>
                             </ul>
                         </p>
                     </div>
                 </div>
+                <img src={mountain} height="400px" className="mountain" alt="mountain"></img>
                 <div >
 
                 </div>
--- src/portfolio.js
+++ src/portfolio.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './portfolio.css';
 import Ethoca from "./images/eth-mastercard.jpg";
 import Chip8 from "./images/chip8.png";
+import Gallery from "./images/gallery.jpg"
 
 export const Portfolio = () => {
     return (
@@ -42,6 +43,23 @@ export const Portfolio = () => {
                     <a href="https://www.github.com" className="hvr-grow">
                         <div className="pic">
                             <img src={Chip8} height="250px" width="450px" alt="chip8"></img>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="text">
+                        <h2 className="pTitle">Gallery</h2>
+                        <div id="smallDivider2"></div>
+                        <h4>Summer 2020</h4>
+                        <p>Gallery is a visual portfolio generator capable of spitting out specific format to your liking for your albums. Gallery is a work in progress of mine with the goal of having custom layout for my photographs and having a simple setup.</p>
+                    </div>
+                </div>
+                <div className="col-md-6 displayR">
+                    <a href="https://www.github.com" className="hvr-grow">
+                        <div className="pic">
+                            <img src={Gallery} height="250px" width="450px" alt="chip8"></img>
                         </div>
                     </a>
                 </div>
